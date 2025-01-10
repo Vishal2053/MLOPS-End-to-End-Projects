32510dafa7a8bb6efb38455a9c8cf3a0939b549d
Vishal More <130221313+Vishal2053@users.noreply.github.com>
2025-01-10 19:01:48 +0530

    model evaluatin added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.datascience.pipeline.data_validation_pipeline import DataValidationTrai
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 logger.info("Welcome to our custom logging")
 
@@ -49,3 +50,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
